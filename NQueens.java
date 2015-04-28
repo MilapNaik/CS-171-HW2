da9a6a4b9bc3fc481a65d94e49ae32103b9ebea4
Milap Naik <milapnaik@gmail.com>
2015-04-27 22:18:34 -0400

    Clarified variables and comments

--- NQueens.java
+++ NQueens.java
@@ -5,16 +5,16 @@ import java.util.Stack;
 
 public class NQueens {
   
-  public static Stack<Integer> s = new Stack<Integer> ();
+  public static Stack<Integer> stack = new Stack<Integer> ();
 
-  private static boolean check(int t) {
-    for(int i = 0; i < s.size(); i++)
+  private static boolean check(int spot) { //Check solution to be viable
+    for(int Queen = 0; Queen < stack.size(); Queen++)// iterate state to see if this spot can threaten other queens
       {
-        if(s.get(i) == t)
+        if(stack.get(Queen) == spot)
           return false;
-        else if(s.size() - s.get(i) == i-t)
+        else if(stack.size() - stack.get(Queen) == Queen-spot)
           return false;
-        else if(s.size() + s.get(i)== i + t)
+        else if(stack.size() + stack.get(Queen)== Queen + spot)
           return false;
       }
     return true;
@@ -29,25 +29,25 @@ public class NQueens {
 
     while(!p){
       for (x = nextQ; x<n; x++){
-       if(check(nextQ)){
-         s.push(nextQ);
-         nextQ=0;
-         break;
-       }
-       nextQ++;
-      }
-       if(x==n){
-         if (s.isEmpty())
-          p=true;
+    	  if(check(nextQ)){ //Check if solution is viable. If not, iterate.
+    		  stack.push(nextQ);
+    		  nextQ=0;
+    		  break;
+    	  }
+    	  nextQ++;
+      } //closes for
+      if(x==n){
+         if (stack.isEmpty())
+        	 p=true;
          else
-           nextQ = s.pop() +1;
-       }
-       if(s.size() == n){
-         printSolution(s);
+        	 nextQ = stack.pop() +1;
+      }
+      if(stack.size() == n){
+         printSolution(stack);
          solution++;
-         nextQ = s.pop() + 1;
-       }
-    }
+         nextQ = stack.pop() + 1;
+      }
+    }//closes while
     return solution;
   }
   
@@ -67,19 +67,19 @@ public class NQueens {
   
   public static void main(String[] args) {
   
-  int n = 8;
+  int number = 8; //Number of queens...change to be entered as an argument later
   
   
-  if (args.length == 1) {
-    n = Integer.parseInt(args[0].trim());
-    if (n < 1) {
+  if (args.length == 1) { //Check perimeters, if number of queens is entered as an argument
+    number = Integer.parseInt(args[0].trim());
+    if (number < 1) {
       System.out.println("Incorrect parameter");
       System.exit(-1);
     }
   }
   
-  int number = solve(n);
-  System.out.println("There are " + number + " solutions to the " + n + "-queens problem.");
+  int solutions = solve(number);
+  System.out.println("There are " + solutions + " solutions to the " + number + "-queens problem.");
  }
   
 }
